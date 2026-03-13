abaca40377b2e4400dbc8cd6f55d8ee391996ab5
Octerry <taaniyu@gmail.com>
2026-03-13 12:58:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Coucou toi qui regarde dans le code
+// 
+// 
+// /‾‾‾‾‾\ /‾‾‾‾‾] [‾‾‾‾‾‾‾‾] |‾‾‾‾‾] |‾‾‾‾‾\ |‾‾‾‾‾\ |‾| /‾/
+// | |‾| | | |‾‾‾   ‾‾|  |‾‾  | |‾‾‾  |     | |     | | |/ /
+// | | | | | |        |  |    |  ___] |     / |     / |  _/
+// | |_| | | |___     |  |    | |___  | |\ \  | |\ \  | |
+// \_____/ \_____]    |__|    |_____] |_| \_\ |_| \_\ |_|
+// MADE ON EARTH BY HUMANS
+
+let sections = document.querySelectorAll('.section');
+let passedSection = sections[0];
+let passedTab = [];
+let weekSection = sections[1];
+let weekTab = [];
+let laterSection = sections[2];
+let laterTab = [];
+
+let addButton = document.getElementById('add_button');
+
+addButton.addEventListener('click', function(){
+    
+})
